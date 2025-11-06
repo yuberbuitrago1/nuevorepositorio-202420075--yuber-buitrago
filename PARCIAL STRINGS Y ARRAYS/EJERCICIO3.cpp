59bb420b74efe3278f21afeba62e31de8af0fe8f
yuberbuitrago1 <yuber.buitrago@uptc.edu.co>
2025-11-06 16:17:57 +0000

    3 hecho

--- PARCIAL STRINGS Y ARRAYS/EJERCICIO3.cpp
+++ PARCIAL STRINGS Y ARRAYS/EJERCICIO3.cpp
@@ -6,25 +6,26 @@ void totalFinesForCity(){
     string placas[6] = {"ABC123", "XYZ789", "LMN456", "JKL321", "QWE654", "TYU999"};
     float multas[6] = {250000, 180000, 300000, 450000, 200000, 120000};
     string ciudades[6] = {"Tunja", "Bogotá", "Tunja", "Medellín", "Cali", "Tunja"};
-    int ciudad;
+    string cityy;
     float totalFines = 0;
 
     cout << "de que ciudad quiere ver las multas:" << endl;
-    cout << "tunja = 0, 2, 5" << endl << "bogota = 1" << endl << "medellin = 3" << endl << "cali = 4" << endl << endl;
+    cout << "Tunja " << endl << "Bogotá" << endl << "Medellín" << endl << "Cali" << endl << endl;
+    cout << "*NOTA: !!ESCRIBIR DE LA MISMA MANERA EN LA QUE SE EN PANTALLA, CON TILDES Y MAYUSCULAS!!*" << endl;
     cout << "ingrese el numero de la ciudad: ";
-    cin >> ciudad;
+    cin >> cityy;
 
-    cout << "multas registradas en la ciudad de " << ciudades[ciudad] << ": " << endl;
+    cout << "multas registradas en la ciudad de " << cityy << ": " << endl;
     for (int i = 0; i < 6; i++)
     {
-        if (ciudades[i] == ciudades[ciudad])
+        if (ciudades[i] == cityy)
         {
             cout << placas[i] << " = " << multas[i] << endl;
             totalFines = totalFines + multas[i];
         }
     }
     
-    cout << "el total de multas a pagar en la ciudad de " << ciudades[ciudad] << " es de: " << totalFines << endl;
+    cout << "el total de multas a pagar en la ciudad de " << cityy << " es de: " << totalFines << endl;
 }
 
 int main(){
