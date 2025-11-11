7d69c77bff4dd67d06f77ee52ba038e7499e8a00
yuberbuitrago1 <yuber.buitrago@uptc.edu.co>
2025-11-11 20:23:56 +0000

    parte1

---
+++ taller 2 manejo de arrays/taller2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+void run(){
+
+    int userOpcion;
+
+    cout "----------------------------------------------" << endl;
+    cout << "sistema de ventas, seleccione una opcion: " << endl;
+    cout << "1. Registrar o reiniciar ventas" << endl;
+    cout << "2. Mostrar todas las ventas" << endl;
+    cout << "3. Modificar una venta" << endl;
+    cout << "4. Buscar una venta" << endl;
+    cout << "5. Mostrar ventas dentro de un rango de precios" << endl;
+    cout << "6. Calcular total de ventas" << endl;
+    cout << "7. Salir" << endl << endl;
+
+    cout << "ingrese una opcion: ";
+    cin >> userOpcion;
+
+    cout "----------------------------------------------" << endl;
+
+}
+
+int main(){
+
+    retun 0;
+}
